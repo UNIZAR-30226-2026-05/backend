00489c9c078e76fa8f749e3fd7711ce5ab66c0a0
Edu <903723@unizar.es>
2026-02-11 10:44:31 +0100

    Creacion tablas modificadas

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# backend
-ALONSO HOLA
+## CREACIÓN DE LA BASE DE DATOS
+Para la creación de la base de datos hemos planteado un esquema entidad relación y obtenido una serie de relaciones. Para organizar mejor cada tabla hemos implementado tres esquemas en la BD:
+- **Juego:** recoge las tablas que contienen información base del juego, es decir, información que una vez añadida no se piensa modificar a no ser que se cambien las características del juego (minijuegos, objetos, personajes, ...)
+
+- **Usuarios:** recoge la información relativa a los usuarios almacenados en el sistema, sus amistades y las sesiones activas
+
+- **Partidas:** compuesta por toda aquella información sobre partidas activas en el servidor. Dicha información es principalmente volátil ya que no se pretende que este almacenada más allá de la duración de la partida
--- creacionTablas.sql
+++ creacionTablas.sql
@@ -1,104 +1,118 @@
-CREATE TABLE USUARIO (
+CREATE SCHEMA USUARIOS;
+CREATE SCHEMA JUEGO;
+CREATE SCHEMA PARTIDAS;
+
+/* ESQUEMA USUARIOS */
+CREATE TABLE USUARIOS.USUARIO (
     nombre VARCHAR(50) PRIMARY KEY,
     password VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE PERSONAJE(
+CREATE TABLE USUARIOS.SESION_ACTIVA(
+    usuario VARCHAR(50) PRIMARY KEY,
+    token VARCHAR(100),
+    ult_acceso timestamp,
+    CONSTRAINT fk_usuario FOREIGN KEY(usuario) REFERENCES USUARIOS.USUARIO(nombre) ON DELETE CASCADE
+);
+
+CREATE TABLE USUARIOS.AMIGOS(
+    usuario1 VARCHAR(50),
+    usuario2 VARCHAR(50),
+    CONSTRAINT fk_usuario1 FOREIGN KEY(usuario1) REFERENCES USUARIOS.USUARIO(nombre) ON DELETE CASCADE,
+    CONSTRAINT fk_usuario2 FOREIGN KEY(usuario2) REFERENCES USUARIOS.USUARIO(nombre) ON DELETE CASCADE,
+    --CONSTRAINT amigos_unicos CHECK (usuario1 < usuario2),
+    CONSTRAINT usuarios_diferentes CHECK (usuario1 <> usuario2),
+    PRIMARY KEY(usuario1, usuario2)
+);
+
+CREATE TABLE USUARIOS.SOLICITUD(
+    solicitante VARCHAR(50),
+    solicitado VARCHAR(50),
+    CONSTRAINT fk_solicitante FOREIGN KEY(solicitante) REFERENCES USUARIOS.USUARIO(nombre) ON DELETE CASCADE,
+    CONSTRAINT fk_solicitado FOREIGN KEY(solicitado) REFERENCES USUARIOS.USUARIO(nombre) ON DELETE CASCADE,
+    --CONSTRAINT amigos_unicos CHECK (solicitante < solicitado),
+    CONSTRAINT usuarios_diferentes CHECK (solicitante <> solicitado),
+    PRIMARY KEY(solicitante,solicitado)
+);
+
+/* ESQUEMA JUEGO */
+CREATE TABLE JUEGO.PERSONAJE(
     nombre VARCHAR(50) PRIMARY KEY,
     habilidad VARCHAR(50) NOT NULL,
     descripcion VARCHAR(200)
 );
 
-CREATE TABLE MINIJUEGO_DINERO(
-    nombre VARCHAR(50) PRIMARY KEY,
-    descripcion VARCHAR(200)
+/*CREATE TYPE tipo_minijuego AS ENUM ('dinero', 'eleccion');*/
+
+CREATE TABLE JUEGO.MINIJUEGO(
+    nombre VARCHAR(50) PRIMARY KEY
 );
 
-CREATE TABLE MINIJUEGO_ELECCION(
+CREATE TABLE JUEGO.MINIJUEGO_DINERO(
     nombre VARCHAR(50) PRIMARY KEY,
-    descripcion VARCHAR(200)
+    descripcion VARCHAR(200),
+    CONSTRAINT fk_minijuego FOREIGN KEY(nombre) REFERENCES JUEGO.MINIJUEGO(nombre)
 );
 
-CREATE TABLE OBJETO(
+CREATE TABLE JUEGO.MINIJUEGO_ELECCION(
+    nombre VARCHAR(50) PRIMARY KEY,
+    descripcion VARCHAR(200),
+    CONSTRAINT fk_minijuego FOREIGN KEY(nombre) REFERENCES JUEGO.MINIJUEGO(nombre)
+);
+
+CREATE TABLE JUEGO.OBJETO(
     nombre VARCHAR(50) PRIMARY KEY,
     precio INTEGER NOT NULL,
     descripcion VARCHAR(200),
     CONSTRAINT precio_pos CHECK ( precio > 0 )
 );
 
-CREATE TABLE CASILLA(
+CREATE TABLE JUEGO.CASILLA(
     numero INTEGER PRIMARY KEY,
     CONSTRAINT numero_correcto CHECK ( numero > 0 and numero < 72 )
 );
 
-CREATE TABLE C_MINI(
+CREATE TABLE JUEGO.C_MINI(
     numero INTEGER PRIMARY KEY,
     minijuego VARCHAR(50) NOT NULL,
-    CONSTRAINT fk_minijuego FOREIGN KEY(minijuego) REFERENCES MINIJUEGO_ELECCION(nombre),
-    CONSTRAINT fk_casilla FOREIGN KEY (numero) REFERENCES CASILLA(numero) ON DELETE CASCADE,
+    CONSTRAINT fk_minijuego FOREIGN KEY(minijuego) REFERENCES JUEGO.MINIJUEGO_DINERO(nombre),
     CONSTRAINT numero_correcto CHECK ( numero > 0 and numero < 72 )
 );
 
-CREATE TABLE C_OBJ(
+CREATE TABLE JUEGO.C_OBJ(
     numero INTEGER PRIMARY KEY,
     objeto VARCHAR(50) NOT NULL,
-    CONSTRAINT fk_objeto FOREIGN KEY(objeto) REFERENCES OBJETO(nombre),
-    CONSTRAINT fk_casilla FOREIGN KEY (numero) REFERENCES CASILLA(numero) ON DELETE CASCADE,
+    CONSTRAINT fk_objeto FOREIGN KEY(objeto) REFERENCES JUEGO.OBJETO(nombre),
     CONSTRAINT numero_correcto CHECK ( numero > 0 and numero < 72 )
 );
 
-CREATE TABLE C_MOV(
+CREATE TABLE JUEGO.C_MOV(
     numero INTEGER PRIMARY KEY,
     movimiento INTEGER NOT NULL,
-    CONSTRAINT fk_casilla FOREIGN KEY (numero) REFERENCES CASILLA(numero) ON DELETE CASCADE,
     CONSTRAINT numero_correcto CHECK ( numero > 0 and numero < 72 )
 );
 
-CREATE TABLE SESION_ACTIVA(
-    usuario VARCHAR(50) PRIMARY KEY,
-    token VARCHAR(100),
-    ult_acceso timestamp,
-    CONSTRAINT fk_usuario FOREIGN KEY(usuario) REFERENCES USUARIO(nombre) ON DELETE CASCADE
-);
-
-CREATE TABLE AMIGOS(
-    usuario1 VARCHAR(50),
-    usuario2 VARCHAR(50),
-    CONSTRAINT fk_usuario1 FOREIGN KEY(usuario1) REFERENCES USUARIO(nombre) ON DELETE CASCADE,
-    CONSTRAINT fk_usuario2 FOREIGN KEY(usuario2) REFERENCES USUARIO(nombre) ON DELETE CASCADE,
-    PRIMARY KEY(usuario1, usuario2)
-);
-
-CREATE TABLE SOLICITUD(
-    solicitante VARCHAR(50),
-    solicitado VARCHAR(50),
-    CONSTRAINT fk_solicitante FOREIGN KEY(solicitante) REFERENCES USUARIO(nombre) ON DELETE CASCADE,
-    CONSTRAINT fk_solicitado FOREIGN KEY(solicitado) REFERENCES USUARIO(nombre) ON DELETE CASCADE,
-    PRIMARY KEY(solicitante,solicitado)
-);
-
-CREATE TABLE PARTIDA_ACTIVA(
+/* ESQUEMA PARTIDAS */
+CREATE TABLE PARTIDAS.PARTIDA_ACTIVA(
     id INTEGER PRIMARY KEY, 
-    jugador VARCHAR(50),
     hay_barrera BOOLEAN[],
     turno INTEGER,
     minijuego VARCHAR(50),
-    personaje VARCHAR(50),
-    CONSTRAINT fk_jugador FOREIGN KEY(jugador) REFERENCES USUARIO(nombre),
-    CONSTRAINT fk_personaje FOREIGN KEY(personaje) REFERENCES PERSONAJE(nombre),
+    ult_resultado INTEGER[],
+    CONSTRAINT fk_minijuego FOREIGN KEY(minijuego) REFERENCES JUEGO.MINIJUEGO(nombre),
     CONSTRAINT turno_positivo CHECK ( turno > 0 )
 );
 
-CREATE TABLE JUGANDO(
+CREATE TABLE PARTIDAS.JUGANDO(
     nombre_jugador VARCHAR(50),
     id_partida INTEGER,
     personaje VARCHAR(50),
     dinero INTEGER NOT NULL,
     casilla INTEGER NOT NULL,
     numero INTEGER NOT NULL,
-    CONSTRAINT fk_partida FOREIGN KEY(id_partida) REFERENCES PARTIDA_ACTIVA(id) ON DELETE CASCADE,
-    CONSTRAINT fk_personaje FOREIGN KEY(personaje) REFERENCES PERSONAJE(nombre),
-    CONSTRAINT fk_jugador FOREIGN KEY(nombre_jugador) REFERENCES USUARIO(nombre),
+    CONSTRAINT fk_partida FOREIGN KEY(id_partida) REFERENCES PARTIDAS.PARTIDA_ACTIVA(id) ON DELETE CASCADE,
+    CONSTRAINT fk_personaje FOREIGN KEY(personaje) REFERENCES JUEGO.PERSONAJE(nombre),
+    CONSTRAINT fk_jugador FOREIGN KEY(nombre_jugador) REFERENCES USUARIOS.USUARIO(nombre),
     CONSTRAINT dinero_pos CHECK ( dinero >= 0 ),
     CONSTRAINT casilla_pos CHECK ( casilla >= 0 ),
     CONSTRAINT numeroCorrecto CHECK ( numero >= 1  and numero <= 4 ),
