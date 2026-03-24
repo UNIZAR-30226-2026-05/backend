2da0df9d6f58fa5bc661ef9fc68d1358b9ee9110
Aritztoteles <aritzcscb@gmail.com>
2026-03-24 09:20:49 +0000

    sesion

--- api/app/sessionmanager.py
+++ api/app/sessionmanager.py
@@ -4,12 +4,12 @@ from routers.usuarios import *
 class SessionManager:
     def __init__(self):
         self.active_users: dict[str, WebSocket] = {}
+        self.state_users: dict[str, str] = {}
 
     async def connect(self, websocket: WebSocket, player_id: str):
-        # BORRAR O COMENTAR ESTA LÍNEA:
-        # await websocket.accept()  <--- ¡Peligro de doble accept!
         
         self.active_users[player_id] = websocket
+        self.state_users[player_id] = "Lobby"
         # Avisamos a sus amigos de que está online
         await self.broadcast_status_to_friends(player_id, "online")
         
@@ -21,12 +21,15 @@ class SessionManager:
 
     async def send_personal_message(self, target_player_id: str, message: dict):
         """Envía un mensaje directo a un usuario (ej. una invitación)"""
-        if target_player_id in self.active_users:
+        if target_player_id in self.active_users and self.state_users[target_player_id] == "online":
             websocket = self.active_users[target_player_id]
             await websocket.send_json(message)
 
     def is_user_online(self, player_id: str) -> bool:
         return player_id in self.active_users
+    
+    async def start_game(self, player_id: str):
+        self.state_users[player_id] = "in_game"
 
     async def broadcast_status_to_friends(self, user: str, status: str):
 
@@ -40,7 +43,7 @@ class SessionManager:
                 await self.send_personal_message(friend_id, {
                     "type": "friend_status_update",
                     "friend_id": user,
-                    "status": status # "online" u "offline"  
+                    "status": status 
                 })
 
 
