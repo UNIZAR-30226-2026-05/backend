4014b5c7aa977930ba2163af701da1c0bd1da09b
Edu <903723@unizar.es>
2026-04-10 16:33:15 +0200

    cambios en docker compose para que coja .env

--- api/app/security.py
+++ api/app/security.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Configuración JWT
-SECRET_KEY = os.getenv("SECRET_KEY", "paquito")
+SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
