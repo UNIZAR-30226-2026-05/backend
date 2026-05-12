6cab300ce8a51f225ede317ff586056557e1dfd5
Alonso de la Calle <a.calle@irradiare.biz>
2026-05-12 12:18:42 +0200

    fix para no meter a jugadores desconectados en el minijuego

--- api/app/gamemanager.py
+++ api/app/gamemanager.py
@@ -580,7 +580,8 @@ class GameManager:
                     minijuego = payload["minijuego"]
                     session.minijuego_actual = minijuego #TEnemos que guardarlo en la sesión también para después saber cómo evaluar las posiciones según el tipo de minijuego
                     descripcion = payload["descripcion"]
-                    session.minijuego_participantes = list(session.players.keys()) # Participan todos los jugadores
+                    #Solo incluimos en el minijuego a los jugadores que están conectados
+                    session.minijuego_participantes =[p_id for p_id, ws in session.players.items() if ws is not None]
                     session.minijuego_tipo = "orden"
 
                     match minijuego:
